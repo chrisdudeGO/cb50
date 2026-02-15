aa3777411b1b763ecdac7eba99614c11f1c2f511
ChristianBertschCEE <christian.bertsch@gmail.com>
2026-02-15 16:56:32 +0100

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['"JetBrains Mono"', 'monospace'],
+        display: ['"Space Grotesk"', 'sans-serif'],
+        body: ['"Inter"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
